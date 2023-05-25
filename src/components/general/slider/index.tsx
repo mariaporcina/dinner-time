27eb2f785ac8c7ca4fc5fad46df5e70d96d3f956
Maria Porcina <maria.porcina.mp@gmail.com>
2023-05-25 19:56:37 -0300

    draft

---
+++ src/components/general/slider/index.tsx
@@ -0,0 +1,38 @@
+import { useState } from 'react';
+import { Alert, FlatList, Image, ImageSourcePropType, ScrollView, Text } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+const banner = require('../../../../assets/images/banner.png');
+
+interface ImageType {
+    id: string;
+    image: ImageSourcePropType;
+}
+
+const Slider = () => {
+    const [images, setImages] = useState<ImageType[]>([
+        {id: '0', image: banner},
+        {id: '1', image: banner},
+        {id: '2', image: banner}
+    ]);
+
+    return (
+        <SafeAreaView>
+            <ScrollView horizontal={true}>
+                {images.map((item, index) => {
+                    return <Image key={index} source={item.image}/>
+                })}
+            </ScrollView>
+            {/* <ScrollView horizontal={true}>
+                <FlatList
+                    data={ images }
+                    renderItem={({ item }) => <Image source={item.image}/>}
+                    keyExtractor={(item) => item.id}
+                />
+            </ScrollView> */}
+        </SafeAreaView>
+    );
+
+}
+
+export default Slider;
